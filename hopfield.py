3c06e632243dff427c9dea41b149ff188a45cfb5
nmscannell <scannell@uwm.edu>
2020-05-01 15:25:27 -0500

    some changes to hopfield

--- hopfield.py
+++ hopfield.py
@@ -54,59 +54,101 @@ def update_input(x, w, asynchronous=False):
     return x
 
 
-def run_experiment(num_correct, r, w, num_iterations=10):
-    for n in range(1, 16):
+def run_experiment(num_correct, r, w, num_iterations=10, num_changes=3):
+    for n in range(1, 4):
         for i in range(num_iterations):
             x0 = flip_pixels(p0, n * 2)
             x1 = flip_pixels(p1, n * 2)
-
-            #im1 = -1*np.reshape(x0, (6, 5))
-            #im2 = -1*np.reshape(x1, (6, 5))
-            #f1 = '0_' + str(n*2) + '.jpg'
-            #f2 = '1_' + str(n*2) + '.jpg'
-            #norm = plt.Normalize(vmin=-1, vmax=1)
-            #cmap = plt.cm.get_cmap('Greys')
-            #im1 = cmap(norm(im1))
-            #im2 = cmap(norm(im2))
-            #plt.imsave(f1, im1)
-            #plt.imsave(f2, im2)
-
             x0 = update_input(x0, w)
             x1 = update_input(x1, w)
-
             if np.array_equal(x0, p0):
                 num_correct[r][n - 1] += 1
             if np.array_equal(x1, p1):
                 num_correct[r][n - 1] += 1
+            '''
+            x2 = flip_pixels(p2, n * 2)
+            x3 = flip_pixels(p3, n * 2)
+            x4 = flip_pixels(p4, n * 2)
+            x5 = flip_pixels(p5, n * 2)
+            x6 = flip_pixels(p6, n * 2)
+            x7 = flip_pixels(p7, n * 2)
+            x8 = flip_pixels(p8, n * 2)
+            x9 = flip_pixels(p9, n * 2)
+            x2 = update_input(x2, w)
+            x3 = update_input(x3, w)
+            x4 = update_input(x4, w)
+            x5 = update_input(x5, w)
+            x6 = update_input(x6, w)
+            x7 = update_input(x7, w)
+            x8 = update_input(x8, w)
+            x9 = update_input(x9, w)
+            if np.array_equal(x2, p2):
+                num_correct[r][n - 1] += 1
+            if np.array_equal(x3, p3):
+                num_correct[r][n - 1] += 1
+            if np.array_equal(x4, p4):
+                num_correct[r][n - 1] += 1
+            if np.array_equal(x5, p5):
+                num_correct[r][n - 1] += 1
+            if np.array_equal(x6, p6):
+                num_correct[r][n - 1] += 1
+            if np.array_equal(x7, p7):
+                num_correct[r][n - 1] += 1
+            if np.array_equal(x8, p8):
+                num_correct[r][n - 1] += 1
+            if np.array_equal(x9, p9):
+                num_correct[r][n - 1] += 1
+            im1 = -1*np.reshape(x0, (6, 5))
+            im2 = -1*np.reshape(x1, (6, 5))
+            f1 = '0_' + str(n*2) + '.jpg'
+            f2 = '1_' + str(n*2) + '.jpg'
+            norm = plt.Normalize(vmin=-1, vmax=1)
+            cmap = plt.cm.get_cmap('Greys')
+            im1 = cmap(norm(im1))
+            im2 = cmap(norm(im2))
+            plt.imsave(f1, im1)
+            plt.imsave(f2, im2)'''
 
 
-correct = np.zeros((5, 15))
+num_pix = 3
+correct = np.zeros((3, num_pix))
 w1 = initialize_weights([p0, p1])
 w2 = initialize_weights([p0, p1, p2])
 w3 = initialize_weights([p0, p1, p2, p3, p4, p5, p6])
-w4 = initialize_weights([p0, p1, p2, p3, p4])
-w5 = initialize_weights([p0, p1, p2, p3, p4, p5, p6, p7, p8, p9])
-run_experiment(correct, 0, w1)
-run_experiment(correct, 1, w2)
-run_experiment(correct, 2, w3)
-run_experiment(correct, 3, w3)
-run_experiment(correct, 4, w3)
+#w4 = initialize_weights([p0, p1, p2, p3, p4, p5, p6, p7, p8, p9])
+run_experiment(correct, 0, w1, num_pix)
+run_experiment(correct, 1, w2, num_pix)
+run_experiment(correct, 2, w3, num_pix)
+#run_experiment(correct, 3, w4, num_pix)
+print(correct)
 correct = np.true_divide(correct, 20)
 error = 1 - correct
 print(correct)
-x = [2*i for i in range(1, 16)]
-plt.plot(x, correct[0], 'r', x, correct[1], 'b-', x, correct[2], 'g')
+num_stored = [2, 3, 7]
+plt.plot(num_stored, correct[:, 0], 'r', num_stored, correct[:, 1], 'b-', num_stored, correct[:, 2], 'g')
 plt.ylabel('Accuracy')
-plt.xlabel('Number of flipped pixels')
-plt.xticks(x)
+plt.xlabel('Number of Stored Digits')
+plt.xticks(num_stored)
 plt.show()
-plt.plot(x, error[0], 'r', x, error[1], 'b-', x, error[2], 'g')
+plt.plot(num_stored, error[:, 0], 'r', num_stored, error[:, 1], 'b-', num_stored, error[:, 2], 'g')
 plt.ylabel('Error')
-plt.xlabel('Number of flipped pixels')
-plt.xticks(x)
+plt.xlabel('Number of Stored Digits')
+plt.xticks(num_stored)
 plt.show()
 
+num_flipped = [2*i for i in range(1, num_pix+1)]
+plt.plot(num_flipped, correct[0], 'r', num_flipped, correct[1], 'b-', num_flipped, correct[2], 'g')
+plt.ylabel('Accuracy')
+plt.xlabel('Number of Flipped Pixels')
+plt.xticks(num_flipped)
+plt.show()
+plt.plot(num_flipped, error[0], 'r', num_flipped, error[1], 'b-', num_flipped, error[2], 'g')
+plt.ylabel('Error')
+plt.xlabel('Number of Flipped Pixels')
+plt.xticks(num_flipped)
+plt.show()
 
+'''
 im1 = -1*np.reshape(p0, (6, 5)).astype('int')
 im2 = -1*np.reshape(p1, (6, 5)).astype('int')
 f1 = '0.jpg'
@@ -117,3 +159,4 @@ im1 = cmap(norm(im1))
 im2 = cmap(norm(im2))
 plt.imsave(f1, im1)
 plt.imsave(f2, im2)
+'''
--- mnist_hopfield.py
+++ mnist_hopfield.py
@@ -1,1 +1,86 @@
-import hopfield
+#import hopfield
+import scipy.io
+digits = scipy.io.loadmat('digits.mat')
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = digits['train']
+#x2 = digits['test'].astype('float32')/255
+y = digits['trainlabels']
+#y2 = digits['testlabels']
+#for i in range(784):
+#    for j in range(5000):
+#        if x[i, j] < 0.5:
+#            x[i, j] = -1
+#        else:
+#            x[i, j] = 1
+#for i in range(784):
+#    for j in range(1000):
+#        if x2[i, j] < 0.5:
+#            x2[i, j] = -1
+#        else:
+#            x2[i, j] = 1
+zeros = []
+ones = []
+twos = []
+threes = []
+fours = []
+fives = []
+sixes = []
+sevens = []
+eights = []
+nines = []
+for i in range(5000):
+    if y[i] == 0:
+        zeros.append(i)
+    elif y[i] == 1:
+        ones.append(i)
+    elif y[i] == 2:
+        twos.append(i)
+    elif y[i] == 3:
+        threes.append(i)
+    elif y[i] == 4:
+        fours.append(i)
+    elif y[i] == 5:
+        fives.append(i)
+    elif y[i] == 6:
+        sixes.append(i)
+    elif y[i] == 7:
+        sevens.append(i)
+    elif y[i] == 9:
+        eights.append(i)
+    elif y[i] == 9:
+        nines.append(i)
+
+ave_0 = x[:,zeros[0]]
+for i in range(1, len(zeros)):
+    ave_0 += x[:,zeros[i]]
+print(ave_0)
+ave_0 = np.true_divide(ave_0, len(zeros))
+for i in range(len(ave_0)):
+    if ave_0[i] < 0.5:
+        ave_0[i] = -1
+    else:
+        ave_0[i] = 1
+ave_1 = x[:,ones[0]]
+for i in range(1, len(ones)):
+    ave_1 += x[:,ones[i]]
+print(ave_1)
+ave_1 = np.true_divide(ave_1, len(ones))
+for i in range(len(ave_1)):
+    if ave_1[i] < 0.5:
+        ave_1[i] = -1
+    else:
+        ave_1[i] = 1
+print(ave_1)
+print(ave_0.shape)
+#print(x.shape)
+im = np.copy(ave_0)
+im = np.reshape(im, (28,28))
+o = np.reshape(x[:,0], (28,28))
+plt.imshow(im)
+plt.show()
+im = np.copy(ave_1)
+im = np.reshape(im, (28,28))
+plt.imshow(im)
+plt.show()
